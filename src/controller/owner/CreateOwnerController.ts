b7fcee834a1fb2e274855311fb9892cbbeb0bf7c
Rafael <rafaelcrd.ribeiro@gmail.com>
2022-10-20 20:02:50 -0300

    prisma added; CreateOwner added

---
+++ src/controller/owner/CreateOwnerController.ts
@@ -0,0 +1,16 @@
+import { Request, Response } from "express";
+
+class CreateOwnerController {
+    async execute(req: Request, resp: Response) {
+        try {
+            const { id, name, idAddress, phone } = req.body
+
+            return resp.status(200).json()
+        } catch (err) {
+            return resp.status(400).json(err)
+        }
+    }
+}
+
+
+export { CreateOwnerController }
